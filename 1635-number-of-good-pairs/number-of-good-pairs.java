ab4ec11169ff64e071a83416c4fd7a71335a0f4e
krishna.0858 <96763880+krishna3163@users.noreply.github.com>
2025-08-28 21:54:29 +0530

    Time: 1 ms (89.34%) | Memory: 41.1 MB (63.56%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,18 +1,13 @@
-
 class Solution {
-   public int numIdenticalPairs(int[] nums) {
-        int i=0,j=0,c=0;     
-        for(i=0;i<nums.length;i++){
-            for(j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j])
-                    c++;;
+    public int numIdenticalPairs(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    k++;
+                }
             }
         }
-        return c;
+        return k;  
     }
 }
-/*    Brute Force (Method 1)
------------------------------
-  Time Complexity: O(n^2)
-  Space Complexity: O(1)
-----------------------------*/
