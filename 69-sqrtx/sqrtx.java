0b404724f14c47d7ecfbac875685d6fe5b896b14
krishna.0858 <96763880+krishna3163@users.noreply.github.com>
2025-09-13 09:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (97.91%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,6 +1,12 @@
 class Solution {
     public int mySqrt(int x) {
-        int y = (int) Math.sqrt(x);
-        return y;
+        for(int i = 0; i< x ; i++){
+            if(i != x){
+              int j = (int) Math.sqrt(x);
+              return j;
+
+            }
+        }
+        return 0;
     }
 }
